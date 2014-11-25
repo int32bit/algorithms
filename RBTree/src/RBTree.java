4e9266c856248696643cbb0a1d0ac10e6896316a
fgp <krystism@gmail.com>
2014-11-25 11:24:27 +0800

    add deletion for RBTRee

--- RBTree/src/RBTree.java
+++ RBTree/src/RBTree.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
-
-
 public class RBTree<E> {
 	private Node<E> root;
 	private int modCount;
@@ -65,6 +65,13 @@ public class RBTree<E> {
 		modCount++;
 		return true;
 	}
+	public boolean remove(E e) {
+		Node<E> removed = find(e);
+		if (removed == null)
+			return false;
+		deleteNode(removed);
+		return true;
+	}
 	protected Node<E> find(E e) {
 		Objects.requireNonNull(e);
 		Node<E> p = root;
@@ -122,6 +129,20 @@ public class RBTree<E> {
 			System.out.print(sb);
 		}
 	}
+	public List<E> toList() {
+		List<E> list = new ArrayList<>(size);
+		dumpToList(root, list);
+		return list;
+	}
+	private void dumpToList(Node<E> p, List<E> list) {
+		if (p == null)
+			return;
+		if (p.left != null)
+			dumpToList(p.left, list);
+		list.add(p.getValue());
+		if (p.right != null)
+			dumpToList(p.right, list);
+	}
 	static final class Node<E> {
 		E value;
 		Node<E> left;
@@ -173,6 +194,42 @@ public class RBTree<E> {
 			while (p.right != null)
 				p = p.right;
 		return p;
+	}
+	private void deleteNode(Node<E> p) {
+		modCount++;
+		size--;
+		// ｐ有两个孩子节点，则使用p的后继代替，然后转化成删除后继节点（后继必然没有左孩子）
+		if (p.left != null && p.right != null) {
+			Node<E> s = successor(p);
+			p.value = s.value;
+			p = s;
+		}
+		Node<E> replacement = (p.left != null ? p.left : p.right);
+		// p有一个孩子节点，则用它的孩子节点替换当前节点
+		if (replacement != null) {
+			replacement.parent = p.parent;
+			if (p.parent == null)// p is root
+				root = replacement;
+			else if (p == p.parent.left)
+				p.parent.left = replacement;
+			else
+				p.parent.right = replacement;
+			p.left = p.right = p.parent = null;
+			if (p.color == BLACK)
+				fixAfterDeletion(replacement);
+		} else if (p.parent == null) { // p没有孩子节点且p是root，则直接删除之。
+			root = null;
+		} else { // p没有孩子节点情况, 把自己当作replacement然后删除之。
+			if (p.color == BLACK)
+				fixAfterDeletion(p);
+			if (p.parent != null) {
+				if (p == p.parent.left)
+					p.parent.left = null;
+				else if (p == p.parent.right)
+					p.parent.right = null;
+				p.parent = null;
+			}
+		}
 	}
 	static <E> Node<E> successor(Node<E> t) {
 		if (t == null)
@@ -317,16 +374,66 @@ public class RBTree<E> {
 		}
 		root.color = BLACK;
 	}
-	public static void main(String[] args) {
-		RBTree<Integer> rb = new RBTree<>();
-		rb.add(5);
-		rb.add(0);
-		rb.add(10);
-		rb.add(-5);
-		rb.add(-1);
-		rb.add(-100);
-		rb.add(2);
-		rb.add(45);
-		rb.print(',', "\n*****************************************\n");
+	private void fixAfterDeletion(Node<E> x) {
+		while (x != root && colorOf(x) == BLACK) {
+			// x 是左孩子情况
+			if (x == leftOf(parentOf(x))) {
+				Node<E> sib = rightOf(parentOf(x));
+				/* 兄弟节点是红色节点，这种情形下我们在N的父亲上做左旋转，
+				 * 把红色兄弟转换成N的祖父，我们接着对调N的父亲和祖父的颜色。
+				 * 完成这两个操作后，尽管所有路径上黑色节点的数目没有改变，
+				 * 但现在N有了一个黑色的兄弟和一个红色的父亲（它的新兄弟是黑色因为它是红色S的一个儿子）
+				 */
+				if (colorOf(sib) == RED) {
+					setColor(sib, BLACK);
+					setColor(parentOf(x), RED);
+					rotateLeft(parentOf(x));
+					sib = rightOf(parentOf(x)); // 此时兄弟节点必为黑色节点。
+				}
+				if (colorOf(leftOf(sib)) == BLACK && colorOf(rightOf(sib)) == BLACK) {
+					setColor(sib, RED);
+					x = parentOf(x);
+				} else {
+					if (colorOf(rightOf(sib)) == BLACK) {
+						setColor(leftOf(sib), BLACK);
+						setColor(sib, RED);
+						rotateRight(sib);
+						sib = rightOf(parentOf(x));
+					}
+					setColor(sib, colorOf(parentOf(x)));
+					setColor(parentOf(x), BLACK);
+					setColor(rightOf(sib), BLACK);
+					rotateLeft(parentOf(x));
+					x = root;
+				}
+			} else {
+				Node<E> sib = leftOf(parentOf(x));
+				 if (colorOf(sib) == RED) {
+	                    setColor(sib, BLACK);
+	                    setColor(parentOf(x), RED);
+	                    rotateRight(parentOf(x));
+	                    sib = leftOf(parentOf(x));
+	                }
+
+	                if (colorOf(rightOf(sib)) == BLACK &&
+	                    colorOf(leftOf(sib)) == BLACK) {
+	                    setColor(sib, RED);
+	                    x = parentOf(x);
+	                } else {
+	                    if (colorOf(leftOf(sib)) == BLACK) {
+	                        setColor(rightOf(sib), BLACK);
+	                        setColor(sib, RED);
+	                        rotateLeft(sib);
+	                        sib = leftOf(parentOf(x));
+	                    }
+	                    setColor(sib, colorOf(parentOf(x)));
+	                    setColor(parentOf(x), BLACK);
+	                    setColor(leftOf(sib), BLACK);
+	                    rotateRight(parentOf(x));
+	                    x = root;
+	                }
+			}
+		}
+		setColor(x, BLACK);
 	}
 }
